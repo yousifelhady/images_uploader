149b2f8da7d7ae38c621cde22e374da9b8197499
Yousif Elhady <yousif.elhady@swvl.com>
2023-01-06 22:59:18 +0200

    removing imgur access key from src code

--- backend/src/config/imgur.js
+++ backend/src/config/imgur.js
@@ -2,7 +2,7 @@ const ImgurStorage = require('multer-storage-imgur');
 const multer = require('multer');
 
 const upload = multer({
-  storage: ImgurStorage({ clientId: 'f0406192c853557' })
+  storage: ImgurStorage({ clientId: 'enter_your_key_here' })
 })
 
 module.exports = upload
