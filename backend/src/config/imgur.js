2a47597039ab59b75b1e0e02dc1cbe35d04ad4c9
Yousif Elhady <yousif.elhady@swvl.com>
2023-01-08 00:55:06 +0200

    adding imgur client id as env var to be set by the user

--- backend/src/config/imgur.js
+++ backend/src/config/imgur.js
@@ -2,7 +2,7 @@ const ImgurStorage = require('multer-storage-imgur');
 const multer = require('multer');
 
 const upload = multer({
-  storage: ImgurStorage({ clientId: 'enter_your_key_here' })
+  storage: ImgurStorage({ clientId: process.env.IMGUR_CLIENT_ID })
 })
 
 module.exports = upload
