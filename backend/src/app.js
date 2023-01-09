b6c1316b6fed30a24117e8a05ef6fb925fe5c47f
Yousif Elhady <yousif.elhady@swvl.com>
2023-01-10 00:54:30 +0200

    adding api documentation using openAPI and swagger ui

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 require("dotenv").config()
 require("./config/connectMongoose")
+const swaggerUi = require("swagger-ui-express")
+const openApi = require("./openAPI")
 const { errors } = require("celebrate")
 const { errorHandlerMiddleware } = require("./middlewares/errorHandling")
 
@@ -12,6 +14,7 @@ app.use(express.json())
 app.use("/", require("./routes/health"))
 app.use("/images", require("./routes/images"))
 app.use("/groups", require("./routes/groups"))
+app.use("/", swaggerUi.serve, swaggerUi.setup(openApi))
 
 /* Middleware to return celebrate validation errors as
    Bad Request (400) instead of Internal Server Error (500)
