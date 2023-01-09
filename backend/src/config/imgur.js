aea23385ff723236d3d78a982141836fa9952543
Yousif Elhady <yousif.elhady@swvl.com>
2023-01-10 01:55:50 +0200

    adding size limit to uploaded files to avoid halting the application when uploading big images, adding file filter to check for images files only

--- backend/src/config/imgur.js
+++ backend/src/config/imgur.js
@@ -1,8 +1,24 @@
 const ImgurStorage = require('multer-storage-imgur');
 const multer = require('multer');
+const boom = require("boom")
 
 const upload = multer({
-  storage: ImgurStorage({ clientId: process.env.IMGUR_CLIENT_ID })
+  storage: ImgurStorage({ clientId: process.env.IMGUR_CLIENT_ID }),
+  limits: {
+    fileSize: parseInt(process.env.IMAGE_MAX_SIZE)
+  },
+  fileFilter: (req, file, cb) => {
+    if (!/.*\.(gif|png|jpg|jpeg)$/.test(file.originalname)) {
+      return cb(boom.badRequest("Only gif, png, jpg, jpeg are supported"), false);
+    }
+    const fileSize = parseInt(req.headers['content-length'])
+    const maxFileSize = parseInt(process.env.IMAGE_MAX_SIZE)
+    const maxFileSizeInBytes = Math.floor(maxFileSize / 1000000) // 1000000 ~ 1 byte
+    if (fileSize > maxFileSize) {
+      return cb(boom.badRequest(`Uploaded file(s) exceeded the allowed size limit which is ${maxFileSize} Bytes ~ ${maxFileSizeInBytes} MB`), false)
+    }
+    cb(null, true);
+  }
 })
 
 module.exports = upload
